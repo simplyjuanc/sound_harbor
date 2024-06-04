4b898ad61f08540271596d813fcefc4f9dcd9777
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2024-06-04 23:16:32 +0100

    remove insecure DB access

--- src/env.js
+++ src/env.js
@@ -20,7 +20,6 @@ export const env = createEnv({
     SUPABASE_URL: z.string().url(),
     SUPABASE_SERVICE_ROLE_KEY: z.string(),
     SUPABASE_JWT_SECRET: z.string(),
-    NODE_TLS_REJECT_UNAUTHORIZED: z.string().optional(),
   },
 
   /**
@@ -51,7 +50,6 @@ export const env = createEnv({
     SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NODE_TLS_REJECT_UNAUTHORIZED: process.env.NODE_TLS_REJECT_UNAUTHORIZED,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
