37ffd2847dc5946995a6e1ded844eddb6c62d48a
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2024-06-08 23:09:26 +0100

    add spotify login bare screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import {SignedIn, SignedOut} from "@clerk/nextjs";
+import {Button} from "~/components/ui/button";
 
 export default function HomePage() {
   return (
@@ -6,7 +7,7 @@ export default function HomePage() {
         <SignedOut>
             <div className="flex flex-col align-middle my-auto">
                 <h1 className="m-auto text-4xl font-bold">
-                  Welcome to the SoundHarbor app.
+                    Welcome to the SoundHarbor app.
                 </h1>
                 <h2 className="m-auto text-3xl font-bold">
                     Please sign in above to continue.
@@ -14,10 +15,23 @@ export default function HomePage() {
             </div>
         </SignedOut>
         <SignedIn>
-            <h1 className="m-auto text-4xl font-bold">
-              Please choose one of the two options above.
-            </h1>
+            <SpotifyLogin />
         </SignedIn>
     </>
-  );
+);
+}
+
+
+function SpotifyLogin() {
+    return <div className="flex flex-col mx-auto justify-center align-middle mt-16 text-center gap-8">
+        <h1 className="my-8 font-semibold text-3xl">Welcome!</h1>
+        <div>
+            <p className="my-6">First things first, we need to get you set up!</p>
+            <p className="my-6">
+                This means that we will first need to connect your Spotify account
+                so that we can get an idea of what kind of music you like.
+            </p>
+        </div>
+        <Button size={"lg"} className="mx-auto">Connect to Spotify</Button>
+    </div>;
 }
