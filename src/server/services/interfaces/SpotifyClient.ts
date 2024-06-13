c30e46745f4d164859c4a2fbf669e026a73dd840
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2024-06-13 22:14:51 +0100

    add typecasting for Spotify client and basic fetch factory

--- src/server/services/interfaces/SpotifyClient.ts
+++ src/server/services/interfaces/SpotifyClient.ts
@@ -2,16 +2,31 @@ import {
     type Album,
     type Albums,
     type Artists,
+    type MaxInt,
+    type Page,
     type Tracks
 } from "@spotify/web-api-ts-sdk";
 
 
-export interface SpotifyClientAPI {
-    getUserTopAlbums: () => Promise<Albums>;
-    getUserTopTracks: () => Promise<Tracks>;
-    getUserTopArtists: () => Promise<Artists>;
+export type RequestResource = "artists" | "tracks" | "albums";
+export type Resource<T> = T extends "artists" ? Artists : T extends "tracks" ? Tracks : T extends "albums" ? Albums : never;
 
-    getArtistTopTracks: () => Promise<Tracks>;
-    searchAlbum:  (album: string, artist: string, accessToken: string) => Promise<Album>;
-    getUserRecommendations: () => Promise<Albums>;
+export interface TopItemsRequest<T extends RequestResource> {
+    type: T;
+    time_range?: "short_term" | "medium_term" | "long_term";
+    limit?: MaxInt<50>;
+    offset?: number;
+}
+
+export type GetUserTopItems<T extends RequestResource> = (request:TopItemsRequest<T>) => Promise<Page<Resource<T>>>;
+
+
+export interface SpotifyClientAPI {
+    getUserTopAlbums: GetUserTopItems<"albums">;
+    getUserTopTracks: GetUserTopItems<"tracks">
+    getUserTopArtists: GetUserTopItems<"artists">
+
+    getArtistTopTracks: () => Promise<Page<Tracks>>;
+    searchAlbum:  (album: string, artist: string, accessToken: string) => Promise<Page<Album>>;
+    getUserRecommendations: () => Promise<Page<Albums>>;
 }
--- src/server/services/main/spotify.ts
+++ src/server/services/main/spotify.ts
@@ -1,39 +1,87 @@
-import {auth, clerkClient} from "@clerk/nextjs/server";
-import type {Artist, MaxInt, Page, Track} from "@spotify/web-api-ts-sdk";
+import {
+    auth,
+    clerkClient,
+    type OauthAccessToken
+} from "@clerk/nextjs/server";
+import {
+    type GetUserTopItems,
+    type RequestResource,
+    type Resource,
+    type SpotifyClientAPI,
+    type TopItemsRequest,
+} from "~/server/services/interfaces/SpotifyClient";
+import type {Page} from "@spotify/web-api-ts-sdk";
+import * as querystring from "node:querystring";
 
 
 const SPOTIFY_BASE_URL = "https://api.spotify.com/";
 const SPOTIFY_API_VERSION = "v1";
 
 
-type getUserTopItems = <T extends "artists" | "tracks">(type: T,
-                                                        time_range?: "short_term" | "medium_term" | "long_term",
-                                                        limit?: MaxInt<50>,
-                                                        offset?: number) => Promise<Page<T extends "artists" ? Artist : Track>>;
 
-/**
- * Retrieves the Spotify access token for the authenticated user.
- * If the user is not authenticated or if there is no access token found, it throws an error.
- *
- * @returns {Promise<string>} A promise that resolves to the Spotify access token.
- * @throws {Error} Will throw an error if the user is not authenticated or if no access token is found.
- */
-export async function getSpotifyAccessToken() {
-    const {userId} = auth();
-    if (!userId) throw new Error("User not found");
 
-    const provider = "oauth_spotify";
-    const clerkResponse = await clerkClient.users.getUserOauthAccessToken(
-        userId,
-        provider
-    );
 
-    const accessToken = clerkResponse.data[0]!
-    if (!accessToken) throw new Error("No access token found.")
+export class SpotifyClient implements SpotifyClientAPI {
 
-    return accessToken
-}
+    private getUserTopItems<T extends RequestResource>(url:string):GetUserTopItems<T> {
+        let headers: ReturnType<typeof this.generateHeader>;
+        this.getAccessToken()
+            .then(accessToken => this.generateHeader(accessToken))
+            .catch(err => console.log(err));
 
-function buildResourcePath(path: string) {
-    return new URL(`${SPOTIFY_API_VERSION}/${path}`, SPOTIFY_BASE_URL)
+        return async (request:TopItemsRequest<T>): Promise<Page<Resource<T>>> => {
+            const builtUrl = this.buildResourcePath(url, request)
+            const response = await fetch(builtUrl, {headers});
+
+            return await response.json() as Page<Resource<T>>;
+        };
+    }
+
+    // TODO: Check the top tracks/artist/album method (one is different from the others)
+    // TODO: Check for the correct URls
+    getUserTopArtists = this.getUserTopItems<"artists">(`me/top/artists`)
+    getUserTopAlbums = this.getUserTopItems<"albums">(`me/top/tracks`)
+    getUserTopTracks = this.getUserTopItems<"tracks">(`me/top/artists`)
+
+
+    async getAccessToken() {
+        const {userId} = auth();
+        if (!userId) throw new Error("User not found");
+
+        const provider = "oauth_spotify";
+        const clerkResponse = await clerkClient.users.getUserOauthAccessToken(
+            userId,
+            provider
+        );
+
+        const accessToken = clerkResponse.data[0]!
+        if (!accessToken) throw new Error("No access token found.");
+        return accessToken
+    }
+
+    buildResourcePath(path: string, query?: TopItemsRequest<RequestResource>) {
+        const preppedQuery = query && this.prepareForQueryString(query);
+        return new URL(
+            `${SPOTIFY_API_VERSION}/${path}${preppedQuery && (`&${querystring.stringify(preppedQuery)}`)}`,
+            SPOTIFY_BASE_URL
+        )
+    }
+
+    prepareForQueryString<T>(obj: T): Record<string, string> {
+        const result: Record<string, string> = {};
+        for (const key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                const value = obj[key];
+                result[key] = String(value);
+            }
+        }
+        return result;
+    }
+
+    generateHeader (accessToken: OauthAccessToken) {
+        return {
+            Authorization: 'Bearer ' + accessToken.token,
+        };
+    };
+
 }
